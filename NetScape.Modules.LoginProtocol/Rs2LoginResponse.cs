aae6d70d33330274aa78567825406d535f0f501e
Jayarrowz <arrowzflame321@gmail.com>
2020-12-21 02:46:02 +0000

    Seperate Login request and response classes

--- NetScape.Modules.LoginProtocol/Rs2LoginResponse.cs
+++ NetScape.Modules.LoginProtocol/Rs2LoginResponse.cs
@@ -2,7 +2,7 @@
 
 namespace NetScape.Modules.LoginProtocol
 {
-    public record Rs2LoginRequest : LoginRequest<Rs2LoginResponse>
+    public class Rs2LoginResponse : LoginResponse<LoginStatus>
     {
     }
 }
