c9f098bc96877de43a883ec49105c06d2bc5d67b
Jayarrowz <arrowzflame321@gmail.com>
2021-05-02 23:13:36 +0100

    Add command message & handler

---
+++ NetScape.Modules.ThreeOneSeven.Game/Messages/Handlers/CommandMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using NetScape.Abstractions.Model.Messages;
+using NetScape.Modules.Messages;
+using Serilog;
+using static NetScape.Modules.Messages.Models.ThreeOneSevenDecoderMessages.Types;
+
+namespace NetScape.Modules.ThreeOneSeven.Game.Messages.Handlers
+{
+    [MessageHandler]
+    public class CommandMessageHandler
+    {
+
+        [Message(typeof(CommandMessage))]
+        public void OnCommand(DecoderMessage<CommandMessage> decoderMessage)
+        {
+            var commandMessage = decoderMessage.Message;
+            Log.Logger.Information("Player {0} executed command {1}", decoderMessage.Player.Username, commandMessage.Command);
+        }
+    }
+}
