33c82691bd74004a9f643cb982785275e90c14aa
JayArrowz <arrowzflame321@gmail.com>
2021-07-31 12:57:50 +0100

    chore: fix js5 priority

--- NetScape.Modules.FourSevenFour.LoginProtocol/Handlers/JS5Decoder.cs
+++ NetScape.Modules.FourSevenFour.LoginProtocol/Handlers/JS5Decoder.cs
@@ -8,7 +8,7 @@ namespace NetScape.Modules.FourSevenFour.LoginProtocol.Handlers
 {
     public class JS5Decoder : ByteToMessageDecoder
     {
-        private int _encryptionKey = 0;
+        private int _encryptionKey;
         protected override void Decode(IChannelHandlerContext context, IByteBuffer input, List<object> output)
         {
             if (input.ReadableBytes > 0 && input.IsReadable())
@@ -17,7 +17,7 @@ namespace NetScape.Modules.FourSevenFour.LoginProtocol.Handlers
                 int opcode = input.ReadByte();
 
                 Log.Logger.Debug("File Request: Opcode: {0}", opcode);
-                if (opcode == 0 || opcode == 1)
+                if (opcode is 0 or 1)
                 {
                     if (input.ReadableBytes < 3)
                     {
@@ -26,11 +26,11 @@ namespace NetScape.Modules.FourSevenFour.LoginProtocol.Handlers
                     }
                     int index = input.ReadByte();
                     int file = input.ReadUnsignedShort();
-                    bool priority = opcode == 0;
+                    bool priority = opcode == 1;
                     Log.Logger.Debug("File Request: Index: {0}, File: {1} Priority: {2}", index, file, priority);
                     _ = context.Channel.WriteAndFlushAsync(new JS5Request(index, file, priority, _encryptionKey));
                 }
-                else if (opcode == 2 || opcode == 3 || opcode == 6)
+                else if (opcode is 2 or 3 or 6)
                 {
                     input.SkipBytes(3);
                 }
--- NetScape.Modules.FourSevenFour.LoginProtocol/JS5Request.cs
+++ NetScape.Modules.FourSevenFour.LoginProtocol/JS5Request.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NetScape.Modules.FourSevenFour.LoginProtocol
+﻿namespace NetScape.Modules.FourSevenFour.LoginProtocol
 {
     public class JS5Request
     {
